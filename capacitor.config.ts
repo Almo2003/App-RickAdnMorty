22b6d0fa1f6e1d12f59bc05d3c2bd36ccc69082f
Almo2003 <164087169+Almo2003@users.noreply.github.com>
2024-07-04 19:32:48 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'App-RickAdnMorty',
+  webDir: 'dist'
+};
+
+export default config;
